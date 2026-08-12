457bf56150871d24d2a804d8efdb2e3f4df0d03d
khb <khb3025@gmail.com>
2026-08-12 16:18:12 +0900

    feat : 메시지 처리 실패 보증 방법
    - 메세지 재시도 설정 :
    @RetryableTopic( attempts ,backOff=@BackOff(delay , multiplier)
    
    - 메세지 처리 실패 Topic 저장 :
    @RetryableTopic(dltTopicSuffix)
    
    - dlt Consumer 작성
      로그시스템 저장
      Slack 등 알림 메세지 전송 설정

--- email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
+++ email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
@@ -2,7 +2,10 @@ package com.example.emailsendconsumer;
 
 import javax.management.RuntimeErrorException;
 
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +15,20 @@ public class EmailSendConsumer {
         topics="email.send",
         groupId = "email-send-group"
     )
+    @RetryableTopic(
+        attempts = "5", // 재시도 횟수
+        backOff = @BackOff(delay = 1000, 
+                           multiplier = 2), // 2배수로 delay 증가 1초 2초 4초 8초 ...
+        dltTopicSuffix = ".dlt" // send.email.dlt  [설정안할 시(기본) = send.email-dlt]
+    )
     public void consumer(String message){
         System.out.println("Kafka로부터 받아온 메시지 : " + message);
         EmailSendMessage emailSendMessage =EmailSendMessage.fromJson(message);
+
+        if(emailSendMessage.getTo().equals("fail@naver.com")){
+            System.out.println("잘못된 이메일 주소로 인해 발송 실패");
+            throw new RuntimeException("잘못된 이메일 주소로 인해 발송 실패");
+        }
 
         // ... 실제 이메일 발송 로직은 생략 ...
         /*
@@ -25,6 +39,5 @@ public class EmailSendConsumer {
         }
         */
         System.out.println("이메일 발송 완료");
-        
     }
 }
---
+++ email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendDltConsumer.java
@@ -0,0 +1,30 @@
+package com.example.emailsendconsumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSendDltConsumer {
+    private final KafkaTemplate kafkaTemplate;
+    
+    public EmailSendDltConsumer(KafkaTemplate kafkaTemplate){
+        this.kafkaTemplate=kafkaTemplate;
+    }
+
+    @KafkaListener(
+        topics = "email.send.dlt",
+        groupId = "email-send-dlt-group"
+    )
+    public void consume(String message){
+        // 로그 시스템에 전송
+        System.out.println("로그 시스템 전송 : " + message);
+
+        // 알림 발송
+        System.out.println("Slack 알림 발송");
+
+        
+
+    }
+
+}
