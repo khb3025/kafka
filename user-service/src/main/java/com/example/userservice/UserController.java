a80766ec666a62a9bfdf76b324a826ca4312e9be
khb <khb3025@gmail.com>
2026-08-14 11:49:11 +0900

    feat : MSA - Kafka 강의 완료

--- user-service/src/main/java/com/example/userservice/UserController.java
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/user")
+@RestController
+@RequestMapping("api/user")
 public class UserController {
     private final UserService userService;
     public UserController(UserService userService) {
